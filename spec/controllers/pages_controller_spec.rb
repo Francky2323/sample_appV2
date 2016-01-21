1edd74609be9bb54d98f5f039c8e89e3c401ad64
Francis Lagueux <francis_lagueux@hotmail.com>
2016-01-20 20:55:28 -0500

    test

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-
+    render_views
+    #Describe -> décris
+    #Home-> est l'action
+    #"returns http success" -> description, si je visite la page home ca devrait fonctionner.
+    #get :home-> on pointe vers la page home, il doit atteindre le contrôleur Pages
   describe "GET #home" do
     it "returns http success" do
       get :home
@@ -14,6 +18,12 @@ RSpec.describe PagesController, type: :controller do
       get :contact
       expect(response).to have_http_status(:success)
     end
+  end
+      describe "GET '#about'" do
+          it "devrait réussir" do
+              get 'about'
+              response.should be_success
+          end
   end
 
 end
