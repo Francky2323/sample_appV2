d081eb0cd8da8240ded74eb94cafbca486ec2488
Francis Lagueux <francis_lagueux@hotmail.com>
2016-01-21 19:52:12 -0500

    Fini les pages statiques

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,16 @@
 class PagesController < ApplicationController
   def home
+      #@titre est une instance de variable permettant d'attribuer un titre approprié à la page.
+      # Ces variables d'instances permettent de lier les actions et les vues.
+      # Ces variables sont automatiquement accessible dans la vue home,contact et about.
+      @titre = "Accueil"
   end
 
   def contact
+      @titre = "Contact"
   end
     
   def about
+      @titre = "À Propos"
   end
 end
